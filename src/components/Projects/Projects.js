664796c5e2b39558535e95e3ab6776e5c0412a41
moba0222 <mohammad.babu.miya@netcracker.com>
2023-06-19 23:00:52 +0530

    new-profile010

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import blog from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -18,12 +18,22 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={blog}
               isBlog={false}
               title="My-Tech-Blogs ( In-progress )"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/babumiya435/Blogs"
               demoLink="https://blogs.babumiya.tech/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={blog}
+              isBlog={false}
+              title="Post-Man ( In-progress )"
+              description="UI build with Next.js and Tailwind Css which is used to post the contnet across social media by one click."
+              ghLink="https://github.com/babumiya435/post-man"
+              demoLink="https://post-man.babumiya.tech/"
             />
           </Col>
         </Row>
