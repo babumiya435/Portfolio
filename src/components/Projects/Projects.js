0125c5770d97f7c2bf057a1d9a9513ad881d5eab
moba0222 <mohammad.babu.miya@netcracker.com>
2023-09-20 09:12:10 +0530

    20-09-2023 - tech blogs starting

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,19 +21,9 @@ function Projects() {
               imgPath={blog}
               isBlog={false}
               title="My-Tech-Blogs ( In-progress )"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from markdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="My Technical Content page build with React Js"
               ghLink="https://github.com/babumiya435/Blogs"
               demoLink="https://blogs.babumiya.tech/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={blog}
-              isBlog={false}
-              title="Post-Man ( In-progress )"
-              description="UI build with Next.js and Tailwind Css which is used to post the contnet across social media by one single click."
-              ghLink="https://github.com/babumiya435/post-man"
-              demoLink="https://post-man.babumiya.tech/"
             />
           </Col>
         </Row>
