8fae41bf6521bacdaa6203aef697b655dc772e75
moba0222 <mohammad.babu.miya@netcracker.com>
2023-10-29 21:25:42 +0530

    learning-tab-added-29-10-2023-002

--- src/components/Content-Details/ContentDetails.js
+++ src/components/Content-Details/ContentDetails.js
@@ -1,35 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
-import { Link } from 'react-router-dom';
 
-function ContentDetails(props) {
+function ContentDetails() {
   return (
+    <div>
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>Content 1</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+          Content 1 Data
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Link style={{ marginLeft: "10px" }}
-           to={props.demoLink}>Content Details
-           </Link>
-        )}
       </Card.Body>
     </Card>
+    </div>
   );
 }
 export default ContentDetails;
