506c81f0df58fa0bbf2a5b5d9639069ab2978501
moba0222 <mohammad.babu.miya@netcracker.com>
2023-11-02 18:29:09 +0530

    02-11-2023 - tech learning starting-002

--- src/components/Content-Details/ContentDetails.js
+++ src/components/Content-Details/ContentDetails.js
@@ -4,6 +4,7 @@ import Card from "react-bootstrap/Card";
 import { useParams } from 'react-router-dom';
 import Paragraph from "../helper-components/Paragraph";
 import { Link } from 'react-router-dom';
+import CodeViewer from "../helper-components/CodeViewer";
 
 function ContentDetails() {
   const { id } = useParams();
@@ -13,12 +14,13 @@ function ContentDetails() {
       <Card className="quote-card-view">
         <Card.Body>
           <blockquote className="blockquote mb-0">
-            <Paragraph />
-            <Link className="link-section" to="/another-page">Go to Another Page</Link>
-            <div>
+          <div>
               <h2>User Profile</h2>
               <p>User ID: {id}</p>
             </div>
+            <Paragraph />
+            <Link className="link-section" to="/another-page">Go to Another Page</Link>
+            <CodeViewer/>
           </blockquote>
         </Card.Body>
       </Card>
