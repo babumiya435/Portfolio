11bf9dbc32641cd8f9a468528f993c22bde8dca4
moba0222 <mohammad.babu.miya@netcracker.com>
2023-12-09 12:36:03 +0530

    09-12-2023 - tech learning starting-004

--- src/components/Tech-Learning/TechLearning.js
+++ src/components/Tech-Learning/TechLearning.js
@@ -1,71 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../Projects/ProjectCards";
 import data from "../../Assets/Data/RawData.json"
 
 const TechLearning = () => {
 
-  useEffect(() => {
+  const [jsonData, setJsonData] = useState(null);
 
-    // Clean-up function
-    return () => {
-      console.log('Functional component will unmount!');
-    };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://raw.githubusercontent.com/babumiya435/Portfolio/main/src/Assets/Data/RawData.json');
+        const data = await response.json();
+        setJsonData(data);
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching JSON data");
+      }
+    }
+    fetchData();
   }, []);
+
+
+  if (!jsonData) {
+    return <p>Loading...</p>;
+  }
+
 
   return (
     <div>
       <Container fluid className="tech-learning-section">
-        {data.map((item, index) => (
+        {jsonData && jsonData.map((item, index) => (
           <Row key={index} style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="tech-learning-card">
-            <ProjectCard
-              imgPath={item.topicImg}
-              isBlog={false}
-              title={item.topicName}
-              description={item.topicDescription}
-              demoLink={"/content-details/" + item.topicNumber}
-            />
-          </Col>
-        </Row>
+            <Col md={4} className="tech-learning-card">
+              <ProjectCard
+                imgPath={item.topicImg}
+                isBlog={false}
+                title={item.topicName}
+                description={item.topicDescription}
+                demoLink={"/content-details/" + item.topicNumber}
+              />
+            </Col>
+          </Row>
         ))}
-        
-        {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="tech-learning-card">
-            <ProjectCard
-              imgPath={blog}
-              isBlog={false}
-              title="Content 1"
-              description="My Technical Content page build with React Js"
-              demoLink="/content-details/1"
-            />
-          </Col>
-        </Row>
-
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="tech-learning-card">
-            <ProjectCard
-              imgPath={blog}
-              isBlog={false}
-              title="Content 2"
-              description="My Technical Content page build with React Js"
-              demoLink="/content-details/2"
-            />
-          </Col>
-        </Row>
-
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="tech-learning-card">
-            <ProjectCard
-              imgPath={blog}
-              isBlog={false}
-              title="Content 3"
-              description="My Technical Content page build with React Js"
-              demoLink="/content-details/3"
-            />
-          </Col>
-        </Row> */}
       </Container>
+      {/* <Container fluid className="tech-learning-section">
+        <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+      </Container> */}
     </div>
   );
 }
