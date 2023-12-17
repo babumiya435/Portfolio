13b9963589b970190b7c3228ed9f9be0606461b0
moba0222 <mohammad.babu.miya@netcracker.com>
2023-12-17 19:23:11 +0530

    17-12-2023 - tech learning starting-003

--- src/components/Content-Details/ContentDetails.js
+++ src/components/Content-Details/ContentDetails.js
@@ -3,9 +3,7 @@ import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import CodeViewer from "../helper-components/CodeViewer";
 import { useLocation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import Accordion from "../helper-components/Accordion";
 
 
@@ -14,20 +12,6 @@ function ContentDetails() {
   const dataFromParent = location.state?.dataFromParent || null;
   const { id } = useParams();
   console.log(dataFromParent);
-  const accordionItems = [
-    {
-      title: 'Item 1',
-      content: 'Content for Item 1',
-    },
-    {
-      title: 'Item 2',
-      content: 'Content for Item 2',
-    },
-    {
-      title: 'Item 3',
-      content: 'Content for Item 3',
-    },
-  ];
 
   return (
     <>
@@ -48,12 +32,6 @@ function ContentDetails() {
               <Container className="paragraph-section container-fluid">
                 <Accordion items={dataFromParent.topicContent} />
               </Container>
-              {/* <Container>
-            <div>
-                <h1>Accordion Example1</h1>
-                <Accordion items={dataFromParent.topicContent} />
-              </div>
-            </Container> */}
             </Container>
             <Container className="paragraph-section container-fluid">
               <div>
