c62f5f52a12e91c50a98eab9715a3532de02443a
moba0222 <mohammad.babu.miya@netcracker.com>
2024-01-09 11:21:44 +0530

    09-01-2024 - tech learning starting-002

--- src/components/helper-components/CodeViewer.js
+++ src/components/helper-components/CodeViewer.js
@@ -1,13 +1,34 @@
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'; // Choose a code style (e.g., docco)
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 
 function CodeViewer(props) {
 
+  const calcMinLineNumberWidth = (code) => {
+    const lines = [...code].reduce((prev, current) => prev + (current === '\n' ? 1 : 0), 1);
+    if (lines > 99) {
+        return "50px";
+    } else if (lines > 9) {
+        return "40px";
+    } else {
+        return null;
+    }
+}
+
   return (
-    <SyntaxHighlighter language="javascript" style={docco}>
-      {props.contentRelatedCode}
-    </SyntaxHighlighter>
+    <div className="container">
+      <SyntaxHighlighter language="javascript"
+        style={atomDark}
+        className={"syntax-highlighter"}
+        useInlineStyles={false}
+        showLineNumbers={true}
+        lineNumberStyle={{ minWidth: calcMinLineNumberWidth(props.contentRelatedCode) }}
+        wrapLines={true}
+      >
+        {props.contentRelatedCode}
+      </SyntaxHighlighter>
+    </div>
   );
 }
 
