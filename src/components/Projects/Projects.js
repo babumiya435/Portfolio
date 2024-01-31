bcbfb7735a8e15148a9c8b8eb7b2342000e2335f
moba0222 <mohammad.babu.miya@netcracker.com>
2024-01-31 22:59:51 +0530

    31-01-2024 - portfolio-002

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,7 @@ function Projects() {
           My Recent <strong className="purple">Project Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently:
+          Here are a few <strong className="purple">Projects </strong>I've worked/ing.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
