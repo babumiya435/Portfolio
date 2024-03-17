e70eb056cb9c41400e522362667f3929cf2d9489
moba0222 <mohammad.babu.miya@netcracker.com>
2024-03-17 16:44:26 +0530

    17-03-2024 - tech learning starting-002

---
+++ src/practiceFiles_BEnglish/businessEnglishPractice.js
@@ -0,0 +1,7 @@
+// ----------------- 17-03-2024 -----------------
+// Business English === Normal English
+
+// setting up   === fix, gear up, prepare, ready, set
+// setting out  === depart, part, set forth, set off, start, start out, take off
+// set down === place down, put down (set down your bags here)
+// let’s touch base === let’s talk
